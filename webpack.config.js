2649f9acd7609bd1f4ececfb87603f11fa28e19a
Jack_EG <JCooper@executive-grapevine.com>
2019-11-01 16:22:13 +0000

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path")
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env'],
+                    plugins: ['transform-object-rest-spread']
+                }
+            }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader','css-loader']
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        publicPath: '/scripts/'
+    },
+    devtool: 'source-map'
+}
